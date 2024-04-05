2d43cc3c70f9bdfa6f2391e0b86d7c268418c3e9
PlayDay <18056374+playday3008@users.noreply.github.com>
2024-04-05 02:45:31 +0200

    refactor(Natives Generator): Reworked
    + Use submodule with natives.json
    + Python Type Safety
    + More readable output

--- scripts/generate_natives.py
+++ scripts/generate_natives.py
@@ -1,25 +1,40 @@
-import json
+# working dir: scripts
+# python ./generate_natives.py
 
-crossmap = {}
-natives = {}
-current_idx = 0
-crossmap_hash_list = []
+import json
+import re
+
+# Source files
+CROSSMAP_FILE = "crossmap.txt"
+NATIVES_FILE = "./gta5-nativedb-data/natives.json"
+# Target files
+CROSSMAP_HEADER_FILE = "../src/invoker/crossmap.hpp"
+NATIVES_HEADER_FILE = "../src/natives.hpp"
+
 
 class Arg:
-    def __init__(self, name: str, type: str):
+    def __init__(self, name: str, type: str) -> None:
         self.name = name
-        self.type = type#.replace("BOOL", "bool")# .replace("Any*", "void*")
+        self.type = type
 
     def __str__(self) -> str:
-        return str(self.type) + " " + str(self.name)
+        return f"{self.type} {self.name}"
 
-class NativeFunc:
-    def __init__(self, namespace: str, name: str, hash: int, args: list[dict], return_type: str):
+
+class NativeFunction:
+    def __init__(
+        self,
+        namespace: str,
+        name: str,
+        hash: int,
+        args: list[dict[str, str]],
+        return_type: str,
+    ) -> None:
         self.namespace = namespace
         self.name = name
         self.hash = hash
         self.args: list[Arg] = []
-        self.return_type = return_type#.replace("BOOL", "bool")# .replace("Any*", "void*")
+        self.return_type = return_type
         self.native_index = -1
         self.fix_vectors = "false"
 
@@ -27,7 +42,7 @@ class NativeFunc:
             self.args.append(Arg(arg["name"], arg["type"]))
             if arg["type"] == "Vector3*":
                 self.fix_vectors = "true"
-    
+
     def get_native_def_str(self) -> str:
         assert self.native_index != -1
 
@@ -39,34 +54,53 @@ class NativeFunc:
                 param_pass += arg.name + ", "
             param_decl = param_decl[:-2]
             param_pass = param_pass[:-2]
-        
+
         return f"FORCEINLINE constexpr {self.return_type} {self.name}({param_decl}) {{ return big::native_invoker::invoke<{self.native_index}, {self.fix_vectors}, {self.return_type}>({param_pass}); }}"
 
+
 class CrossmapEntry:
-    def __init__(self, translated_hash: int):
+    def __init__(self, translated_hash: int) -> None:
         self.hash = translated_hash
         self.native_index = -1
 
-def load_crossmap_data():
-    global crossmap
 
-    data = open("crossmap.txt").readlines()
+def load_crossmap_data(file: str) -> dict[int, CrossmapEntry]:
+    crossmap: dict[int, CrossmapEntry] = {}
+
+    data = open(file).readlines()
     for item in data:
         translation = item.split(",")
         crossmap[int(translation[0], 16)] = CrossmapEntry(int(translation[1], 16))
 
-def load_natives_data():
-    global natives
+    return crossmap
 
-    data = json.load(open("natives.json"))
+
+def load_natives_data(file: str) -> dict[str, list[NativeFunction]]:
+    natives: dict[str, list[NativeFunction]] = {}
+
+    data = json.load(open(file))
     for ns, natives_list in data.items():
         natives[ns] = []
         for hash_str, native_data in natives_list.items():
-            natives[ns].append(NativeFunc(ns, native_data["name"], int(hash_str, 16), native_data["params"], native_data["return_type"]))
+            natives[ns].append(
+                NativeFunction(
+                    ns,
+                    native_data["name"],
+                    int(hash_str, 16),
+                    native_data["params"],
+                    native_data["return_type"],
+                )
+            )
 
-def allocate_indices():
-    global current_idx, crossmap_hash_list
+    return natives
 
+
+def allocate_indices(
+    natives: dict[str, list[NativeFunction]], crossmap: dict[int, CrossmapEntry]
+) -> list[int]:
+
+    current_idx = 0
+    crossmap_hash_list: list[int] = []
     for _, n in natives.items():
         for native in n:
             hash = native.hash
@@ -76,18 +110,41 @@ def allocate_indices():
                 crossmap_hash_list.append(crossmap[hash].hash)
                 current_idx += 1
 
+    return crossmap_hash_list
 
-def write_crossmap_header():
-        open("crossmap.hpp", "w+").write(f"""#pragma once
-#include <script/scrNativeHandler.hpp>
 
-namespace big
-{{
-	constexpr std::array<rage::scrNativeHash, {len(crossmap_hash_list)}> g_crossmap = {{{",".join([f"0x{x:X}" for x in crossmap_hash_list])}}};
-}}
-""")
+def write_crossmap_header(file: str, crossmap_hash_list: list[int]) -> None:
+    crossmap_hash_list_formatted = re.sub(
+        r" \x7f",
+        "",
+        ", ".join(
+            [
+                ("\x7f\n\t\t" if i != 0 and i % 8 == 0 else "") + f"0x{x:016X}"
+                for i, x in enumerate(crossmap_hash_list)
+            ]
+        ),
+    )
 
-def write_natives_header():
+    replacement_code = re.sub(
+        r"^\s{8}",
+        "",
+        f"""#pragma once
+        #include <script/scrNativeHandler.hpp>
+        
+        namespace big
+        {{
+        	constexpr std::array<rage::scrNativeHash, {len(crossmap_hash_list)}> g_crossmap = {{
+                {crossmap_hash_list_formatted}
+            }};
+        }}
+        """,
+        flags=re.MULTILINE,
+    )
+
+    open(file, "w+").write(replacement_code)
+
+
+def write_natives_header(file: str, natives: dict[str, list[NativeFunction]]) -> None:
     natives_buf = ""
     natives_index_buf = ""
 
@@ -100,24 +157,38 @@ def write_natives_header():
             natives_buf += f"\t{nat_data.get_native_def_str()}\n"
             natives_index_buf += f"\t{nat_data.name} = {nat_data.native_index},\n"
         natives_buf += "}\n\n"
-    
+
+    natives_index_buf = natives_index_buf[:-2]
     natives_buf = natives_buf[:-2]
 
-    open("../natives.hpp", "w+").write(f"""#pragma once
-#include "invoker/invoker.hpp"
+    replacement_code = re.sub(
+        r"^\s{8}",
+        "",
+        f"""#pragma once
+        #include "invoker/invoker.hpp"
+        
+        enum class NativeIndex
+        {{
+        {natives_index_buf}
+        }};
+        
+        // clang-format off
+        {natives_buf}
+        // clang-format on
+        """,
+        flags=re.MULTILINE,
+    )
 
-// clang-format off
-enum class NativeIndex
-{{
-{natives_index_buf}}};
+    open(file, "w+").write(replacement_code)
 
-{natives_buf}
-// clang-format on
-""")
-    
+
+def main() -> None:
+    crossmap = load_crossmap_data(CROSSMAP_FILE)
+    natives = load_natives_data(NATIVES_FILE)
+    crossmap_hash_list = allocate_indices(natives, crossmap)
+    write_crossmap_header(CROSSMAP_HEADER_FILE, crossmap_hash_list)
+    write_natives_header(NATIVES_HEADER_FILE, natives)
+
+
 if __name__ == "__main__":
-    load_crossmap_data()
-    load_natives_data()
-    allocate_indices()
-    write_crossmap_header()
-    write_natives_header()
+    main()
